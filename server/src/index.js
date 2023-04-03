c7be943d0333791620490c16aa12399592663bcf
AfiqRosli <afiq_rosli@live.com>
2023-04-03 22:57:15 +0800

    change port

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || process.env.WEBSITES_PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
